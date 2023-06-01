46ff074895e3987d90e83cfaca24306dcdbdfa76
hurenren <hurenfa@gmail.com>
2023-06-01 22:31:44 +0800

    fest: typeORM builder连接查询

--- src/utils/db.helper.ts
+++ src/utils/db.helper.ts
@@ -6,9 +6,8 @@ export const conditionUtils = <T>(
     obj: Record<string, unknown>,
 ) => {
     _.forEach(obj, (value, key) => {
-        if (!_.isNil(value)) {
+        if (!_.isNil(value))
             queryBuilder.andWhere(`${key}=:${key}`, { [key]: value });
-        }
     });
 
     return queryBuilder;
